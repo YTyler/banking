4d0ab943b9a340970758d190a0c8a6e59ac6e104
Tyler Yates <tyates907@gmail.com>
2022-01-27 13:25:11 -0800

    create User entity

---
+++ src/main/java/com/ytyler/banking/entities/User.java
@@ -0,0 +1,68 @@
+package com.ytyler.banking.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private long user_id;
+    private String password;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String password, String role) {
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(long user_id, String password, String role) {
+        this.user_id = user_id;
+        this.password = password;
+        this.role = role;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + user_id +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
