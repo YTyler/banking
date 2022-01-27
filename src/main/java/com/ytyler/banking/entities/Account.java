ec61ecd27bb2fd50692eba1de156d2de6d7fc48a
Tyler Yates <tyates907@gmail.com>
2022-01-27 12:07:57 -0800

    create account entity

---
+++ src/main/java/com/ytyler/banking/entities/Account.java
@@ -0,0 +1,68 @@
+package com.ytyler.banking.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Account {
+    @Id
+    @SequenceGenerator(
+            name = "account_sequence",
+            sequenceName = "account_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "account_sequence"
+    )
+    private long account_number;
+    private long customer_id;
+    private long balance;
+
+    public Account() {
+    }
+
+    public Account(long customer_id, long balance) {
+        this.customer_id = customer_id;
+        this.balance = balance;
+    }
+
+    public long getAccount_number() {
+        return account_number;
+    }
+
+    public void setAccount_number(long account_number) {
+        this.account_number = account_number;
+    }
+
+    public long getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(long customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    public Account(long account_number, long customer_id, long balance) {
+        this.account_number = account_number;
+        this.customer_id = customer_id;
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "account_number=" + account_number +
+                ", customer_id=" + customer_id +
+                ", balance=" + balance +
+                '}';
+    }
+}
