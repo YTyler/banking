76f9a7d31b7f3b20782cd0d84109765594e1b43c
Tyler Yates <tyates907@gmail.com>
2022-02-02 13:40:34 -0800

    finish user services/directories

---
+++ src/main/java/com/ytyler/banking/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.ytyler.banking.controller;
+
+import com.ytyler.banking.entity.Customer;
+import com.ytyler.banking.entity.User;
+import com.ytyler.banking.exception.ResourceNotFoundException;
+import com.ytyler.banking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@CrossOrigin
+public class UserController {
+
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        return new ResponseEntity<>(userService.readAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path="{id}")
+    public ResponseEntity<Object> getById(@PathVariable("id") Long id) {
+        try {
+            return new ResponseEntity<>(userService.readById(id), HttpStatus.OK);
+        } catch(ResourceNotFoundException e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(e, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<User> postUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.create(user), HttpStatus.OK);
+    }
+
+    @PutMapping(path="{id}")
+    public ResponseEntity<Object> putUser(@PathVariable Long id, @RequestBody User user) {
+        try{
+            return new ResponseEntity<>(userService.update(id, user), HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(e, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(path="{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable long id) {
+        try {
+            return new ResponseEntity<>(userService.delete(id), HttpStatus.OK);
+        } catch(ResourceNotFoundException e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(e, HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/com/ytyler/banking/service/UserService.java
@@ -0,0 +1,68 @@
+package com.ytyler.banking.service;
+
+import com.ytyler.banking.entity.User;
+import com.ytyler.banking.exception.ResourceNotFoundException;
+import com.ytyler.banking.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepo userRepo;
+
+    @Autowired
+    public UserService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    //get all Users
+    public List<User> readAll() {
+        return userRepo.findAll();
+    }
+
+    //get a User by id
+    public User readById(Long id) throws ResourceNotFoundException {
+        Optional<User> userOptional = userRepo.findById(id);
+        if(userOptional.isEmpty()) {
+            throw new ResourceNotFoundException("User with id: " + id + " was not found");
+        } else {
+            return userOptional.get();
+        }
+    }
+
+    //create a new User
+    public User create(User user) {
+        return userRepo.save(user);
+    }
+
+    //update an existing User
+    @Transactional
+    public User update(long id, User user) throws ResourceNotFoundException {
+        Optional<User> userOptional = userRepo.findById(id);
+        if (userOptional.isEmpty()) {
+            throw new ResourceNotFoundException("User with id: " + id + " was not found");
+        } else {
+            User userEdit = userOptional.get();
+            //if a particular value is given replace it, else keep original value
+            userEdit.setPassword(user.getPassword().length() > 0 ? user.getPassword() : userEdit.getPassword());
+            userRepo.save(userEdit); //save and return the edited user
+            return userEdit;
+        }
+    }
+
+    //delete a User
+    public User delete(Long id) throws ResourceNotFoundException{
+        Optional<User> userOptional = userRepo.findById(id);
+        if (userOptional.isEmpty()) {
+            throw new ResourceNotFoundException("User with id: " + id + " was not found");
+        }
+        User user = userOptional.get();
+        userRepo.deleteById(id);
+        return user;
+    }
+}
