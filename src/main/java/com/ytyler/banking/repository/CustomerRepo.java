899ee34592212b555f30407dd3042290c37c1d8e
Tyler Yates <tyates907@gmail.com>
2022-02-03 20:08:35 -0800

    +get by pan repo query

--- src/main/java/com/ytyler/banking/repository/CustomerRepo.java
+++ src/main/java/com/ytyler/banking/repository/CustomerRepo.java
@@ -2,8 +2,13 @@ package com.ytyler.banking.repository;
 
 import com.ytyler.banking.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
+    @Query(value = "SELECT * FROM CUSTOMER c WHERE c.pan = ?1", nativeQuery = true)
+    Optional<Customer> findByPan(long pan);
 }
